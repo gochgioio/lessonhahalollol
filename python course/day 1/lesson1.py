ce946e3146bb005012718faffb55b2f79f252a1a
gochgioio <145489673+gochgioio@users.noreply.github.com>
2023-10-17 16:29:04 +0400

    Add files via upload

---
+++ python course/day 1/lesson1.py
@@ -0,0 +1,45 @@
+from turtle import*
+
+
+
+
+width(10)
+
+color("brown")
+begin_fill()
+forward (200)
+left (90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+forward(70)
+color("black")
+begin_fill()
+left (90)
+
+forward (120)
+right (90)
+
+forward (60)
+right (90)
+
+forward(120)
+end_fill()
+color("red")
+penup()
+goto(200, 200)
+pendown()
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+exitonclick()
